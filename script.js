0f47d25a338c5c1fb4160d1a4fb7ad39f2061bc8
Jefferson França <jefferson.vieiraf@hotmail.com>
2022-09-12 22:37:14 -0300

    feat: array to store board values

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 const gameBoard = (() => {
     const gameBoard = document.getElementById('game-board');
+    const boardValues = new Array(9);
 
     const makeBoard = () => {
         for(let i=0; i<9; i++){
             const div = document.createElement('div');
             div.classList.add('board-square');
+            div.textContent = boardValues[i];
             gameBoard.appendChild(div);
         }
     }
